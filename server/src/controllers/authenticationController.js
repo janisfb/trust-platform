003089c960899414319253e51c74963c67c44d84
Janis Büse <JanisBuese@gmail.com>
2020-11-30 21:38:48 +0100

    added auth

--- server/src/controllers/authenticationController.js
+++ server/src/controllers/authenticationController.js
@@ -1,7 +1,66 @@
-module.exports = {
-  register (req, res) {
-    res.send({
-      message: `Register: ${req.body.email}!`
+const { User } = require('../models')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('../config/config')
+
+function jwtSignUser (user) {
+  const ONE_WEEK = 60 * 60 * 24 * 7
+  return jwt.sign({ id: user.id }, config.authentication.jwtSecret, {
+    expiresIn: ONE_WEEK
+  })
+}
+
+exports.register = async (req, res, next) => {
+  // Hashing
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(req.body.password, salt)
+  const user = {
+    email: req.body.email,
+    password: hashedPassword
+  }
+  console.log('123')
+  try {
+    const userRecord = await User.create(user)
+    user.id = userRecord.id
+    delete user.password
+    return user
+  } catch (error) {
+    error.statusCode = 403
+    throw error
+  }
+}
+
+exports.login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        email: req.body.email
+      }
     })
+    if (!user) {
+      throw Object.assign(
+        new Error('The login information was incorrect'),
+        { statusCode: 403 }
+      )
+    }
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if (!validPassword) {
+      throw Object.assign(
+        new Error('The login information was incorrect'),
+        { statusCode: 403 }
+      )
+    }
+
+    const userJson = user.toJSON()
+    delete userJson.password
+    return {
+      user: userJson,
+      token: jwtSignUser(user)
+    }
+  } catch (error) {
+    error.statusCode = error.statusCode || 500
+    console.error(error)
+    throw error
   }
 }
--- server/src/routes/api/auth.js
+++ server/src/routes/api/auth.js
@@ -1,11 +1,23 @@
 const Router = require('express').Router
+const authenticationController = require('../../controllers/authenticationController')
+const authenticationPolicyMiddleware = require('../../middleware/authenticationPolicyMiddleware')
 
 module.exports = Router({ mergeParams: true })
-  .post('/register', async (req, res, next) => {
+  .post('/register', authenticationPolicyMiddleware.register, async (req, res, next) => {
     try {
-      res.send({
-        message: `Register: ${req.body.email}!`
+      const user = await authenticationController.register(req, res, next)
+      res.send(user)
+    } catch (error) {
+      res.status(error.statusCode).json({
+        status: error.status,
+        message: error.message
       })
+    }
+  })
+  .post('/login', async (req, res, next) => {
+    try {
+      const userToken = await authenticationController.login(req, res, next)
+      res.send(userToken)
     } catch (error) {
       next(error)
     }
